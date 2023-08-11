7a25c93051e81bea010ff969777d4d5ff9af0d2b
John Doe <johndoe@example.com>
2023-08-12 01:22:05 +0200

    ...

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    let i = 0
+    while(i < this.length && this.items[i]<item) {
+      i++
+    }
+    this.items.splice(i, 0, item)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos<0 || pos >= this.length) {
+      throw new Error ("OutOfBounds")
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } 
+    return this.items[this.length-1]
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("Empty SortedList");
+    } 
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0
+    if (this.length === 0) {
+      return 0
+    } 
+    this.items.forEach(element => {
+      sum += element
+    });
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("Empty SortedList");
+    } 
+    return this.sum() / this.length
+  }
 }
 
 module.exports = SortedList;
